1f1e05582c02c35a22f8bf776847a9db215222f0
Stef533 <petreastefaniulian@gmail.com>
2023-09-25 11:08:36 +0200

    objects

---
+++ 3. javascript/live/object.js
@@ -0,0 +1,13 @@
+let person = {
+    name: 'stefan',
+    age: 21
+} 
+/* console.log(person.name + " ha " + person.age + " anni"); */
+
+person.name = 'Marco';
+person.surname = 'Rossi';
+delete person.name;
+console.log(person);
+
+
+ 
