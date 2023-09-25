c94af99b1d9b446c8e86cfb4afa181b2f5495570
MarcoG <baldogiallo@gmail.com>
2023-09-25 10:00:36 +0200

    esercizi array

---
+++ 3. javascript/live/arrays.js
@@ -0,0 +1,17 @@
+const words = ["spray", "limit", "elite", "exuberant", "present"];
+
+/* Esercizio 1
+ words.forEach((word)=> {
+    console.log(word)
+    console.log(word.toUpperCase())
+})
+
+ */
+/* Esercizio 2 */
+let lastElement = words.pop();
+console.log(words);
+console.log(lastElement);
+
+let firstElement = words.shift() 
+console.log(firstElement)
+
