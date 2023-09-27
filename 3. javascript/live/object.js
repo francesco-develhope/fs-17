9a6ec7cd1d86d53da325023fb20b83a2d9f70f19
Stef533 <petreastefaniulian@gmail.com>
2023-09-27 10:49:13 +0200

    keys

--- 3. javascript/live/object.js
+++ 3. javascript/live/object.js
@@ -4,10 +4,12 @@ let person = {
 } 
 /* console.log(person.name + " ha " + person.age + " anni"); */
 
-person.name = 'Marco';
+/* person.name = 'Marco';
 person.surname = 'Rossi';
-delete person.name;
-console.log(person);
+delete person.name; */
+let keys = Object.keys(person);
 
-
- 
+for(let key of Object.keys(person)){
+    let value = person[key];
+    console.log(value);
+}
