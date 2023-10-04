308b8e051e806a95f3bf37dcc3411ebf8d702575
Bjorn Agbalog <bjornlouie98@gmail.com>
2023-10-04 10:24:57 +0200

    excercise classes

--- 3. javascript/live/classes.js
+++ 3. javascript/live/classes.js
@@ -13,3 +13,21 @@ class Person {
 const person = new Person ("Marco", "Bianchi", 26)
 
 console.log(person.toString())
+
+class Student extends Person{
+    constructor(name, surname, age, graduationTimestamp, courses){
+        super (name,surname,age);
+        this.graduationTimestamp=graduationTimestamp;
+        this.courses = courses;
+    }
+    addCourses(arr){
+        this.courses.push(...arr);
+        
+    }
+
+}
+
+const student= new Student('Mario','Rossi','26',new Date('2024-05-18'), ['math']);
+console.log(student.graduationTimestamp);
+student.addCourses(['science','italiano']);
+console.log(student.courses)
