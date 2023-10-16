108e9942a0252a37f3256f4cef1acd4bbf5e2ca9
Bjorn Agbalog <bjornlouie98@gmail.com>
2023-10-16 10:41:37 +0200

    exercise fetch

--- 3. javascript/live/fetch.js
+++ 3. javascript/live/fetch.js
@@ -1,4 +1,30 @@
-fetch('https://jsonplaceholder.typicode.com/todos/')
+/* fetch('https://jsonplaceholder.typicode.com/todos/')
 .then(res => res.json())
 .then(val => console.log(val))
-.catch(error => console.error(error))
+.catch(error => console.error(error)) */
+
+
+//Async / await fetch
+/* async function asynFetch(){
+  try {
+    const result = await fetch('https://jsonplaceholder.typicode.com/todos/');
+    const data = await result.json();
+    console.log(data);
+    return data
+  } catch (error) {
+    console.log(error)
+  }
+}
+asynFetch() */
+
+//Check the status
+/* fetch('https://jsonplaceholder.typicode.com/todos/')
+  .then((res) => {
+    if(res.status === 200){
+    return res.json();
+    } else {
+      throw new Error('This an error')
+    }
+  })
+  .then(val => console.log(val))
+  .catch(error => console.error(error)) */
